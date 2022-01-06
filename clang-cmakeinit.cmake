05f1d1c2517788aa5f16138bddfb9af8921fc948
azukibar0713 <cervelo0713@gmail.com>
2022-01-06 18:14:59 +0900

    clang

--- clang-cmakeinit.cmake
+++
@@ -1,3 +0,0 @@
-# cmake -C ../clang-cmakeinit.cmake ..
-set(CMAKE_C_COMPILER "/usr/bin/clang" CACHE string "clang compiler" FORCE)
-set(CMAKE_CXX_COMPILER "/usr/bin/clang++" CACHE string "clang++ compiler" FORCE)
